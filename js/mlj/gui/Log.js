2c74fcf63062d883eeeb802ab011983fbc4adc37
kchemorion <kchemorion@gmail.com>
2024-03-11 11:30:57 +0100

    v101

--- js/mlj/gui/Log.js
+++ js/mlj/gui/Log.js
@@ -1,16 +1,19 @@
 (function (component) {
 
     var _Log = function () {
-        var _this = this;
+        // Define modal content with a custom header bar for the close button
+        var modalContent = `
+         <div style="position: absolute; top: 0; right: 0; height: 40px; width: 100%; border-bottom: 1px solid #ccc;">
+                <button type="button" class="modal-close-btn" style="position: absolute; top: 5px; right: 10px; border: 1px solid #aaa; background: #fff; font-size: 1rem; cursor: pointer; color: #000; padding: 5px 10px; border-radius: 5px;">&times;</button>
+            </div>
+            <img src='IVDspineView-1.png' style='width:100%; display: block; margin-top: 40px;'>
+        `;
 
-        // Prepare the modal content
-        var modalContent = "<img src='IVDspineView-1.png' style='width:100%;'>"; // Customize this content
-        
-        // Create the modal but don't show it yet
-        var _$logModal = $('<div id="logModal" title="Log Details">' + modalContent + '</div>').hide();
+        // Create the modal with relative positioning
+        var _$logModal = $('<div id="logModal" style="position: relative; padding-top: 40px;">' + modalContent + '</div>').hide();
         $('body').append(_$logModal);
 
-        // Button setup to trigger the modal
+        // Setup the button to trigger the modal
         var _$logButton = $('<button/>', {
             type: 'button',
             class: 'btn btn-primary',
@@ -18,15 +21,15 @@
         }).click(function () {
             _$logModal.dialog({
                 modal: true,
-                width: '920px', // or 'auto'
+                width: '920px',
                 height: 'auto',
-                open: function() {
-                    // Optional: Any action to take when the modal opens
-                },
-                close: function() {
-                    // Optional: Any action to take when the modal closes
+                autoOpen: false,
+                open: function(event, ui) {
+                    $('.ui-dialog-titlebar').hide();
                 }
-            }); 
+            });
+
+            _$logModal.dialog('open');
         });
 
         // Append the button to the widget wrapper
@@ -35,6 +38,11 @@
             $wrap.append(_$logButton);
             return $wrap;
         };
+
+        // Add functionality to close the modal when the custom close button is clicked
+        _$logModal.on('click', '.modal-close-btn', function() {
+            _$logModal.dialog('close');
+        });
     };
 
     MLJ.extend(MLJ.gui.Widget, _Log);
